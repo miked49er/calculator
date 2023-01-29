4576784d27972f91c390c9c54ee78cd8d362086b
Mike Deiters <mike@deiters.me>
2023-01-28 19:52:07 -0500

    Added Op as a type for text and click event

--- src/lib/atoms/CircleButton/CircleButton.tsx
+++ src/lib/atoms/CircleButton/CircleButton.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
 import { Button } from '@mui/material';
 import styles from './CircleButton.module.scss';
+import { Op } from '../../pages/Calculator/Operations';
 
 type Props = {
-  text: string;
+  text: string | Op;
   className?: string;
+  click: () => void;
 };
 
 const {btn} = styles;
-export const CircleButton = ({className, text}: Props) => {
+export const CircleButton = ({className, click, text}: Props) => {
   return (
-    <Button className={[btn, className].join(' ')} variant='contained'>{text}</Button>
+    <Button
+      className={[btn, className].join(' ')}
+      onClick={click}
+      variant='contained'
+    >
+      {text}
+    </Button>
   );
 };
