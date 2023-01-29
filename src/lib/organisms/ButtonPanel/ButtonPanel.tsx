7dc0bb02ccd5d8f9e0ecf00035b9557afe5cc171
Mike Deiters <mike@deiters.me>
2023-01-28 19:52:57 -0500

    Basic functionality implemented

--- src/lib/organisms/ButtonPanel/ButtonPanel.tsx
+++ src/lib/organisms/ButtonPanel/ButtonPanel.tsx
@@ -1,31 +1,64 @@
 import * as React from 'react';
 import styles from './ButtonPanel.module.scss';
 import { CircleButton } from '../../atoms/CircleButton/CircleButton';
+import { Op } from '../../pages/Calculator/Operations';
 
 type Props = {
-  onOutput: () => void;
+  onNumberPress: (num: string) => void;
+  onOpSelect: (op: Op) => void;
+  onReset: () => void;
 };
 
 const {btnPanel, controls, ops, numbers, zero} = styles;
-export const ButtonPanel = ({onOutput}: Props) => {
-  const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
+export const ButtonPanel = ({onNumberPress, onOpSelect, onReset}: Props) => {
+  const nums: string[] = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
   return (
     <div className={btnPanel}>
       <div className={controls}>
-        <CircleButton text='AC'></CircleButton>
-        <CircleButton text='+ / -'></CircleButton>
-        <CircleButton text='%'></CircleButton>
+        <CircleButton
+          text='AC'
+          click={onReset}
+        />
+        <CircleButton
+          text='+ / -'
+          click={() => {
+          }}
+        />
+        <CircleButton
+          text='%'
+          click={() => {
+          }}
+        />
       </div>
       <div className={ops}>
-        <CircleButton text='÷'></CircleButton>
-        <CircleButton text='×'></CircleButton>
-        <CircleButton text='-'></CircleButton>
-        <CircleButton text='+'></CircleButton>
-        <CircleButton text='='></CircleButton>
+        <CircleButton
+          text={Op.DIVIDE}
+          click={() => onOpSelect(Op.DIVIDE)}
+        />
+        <CircleButton
+          text={Op.MULTIPLY}
+          click={() => onOpSelect(Op.MULTIPLY)}
+        />
+        <CircleButton
+          text={Op.SUBTRACT}
+          click={() => onOpSelect(Op.SUBTRACT)}
+        />
+        <CircleButton
+          text={Op.ADD}
+          click={() => onOpSelect(Op.ADD)}
+        />
+        <CircleButton
+          text={Op.EQUALS}
+          click={() => onOpSelect(Op.EQUALS)}
+        />
       </div>
       <div className={numbers}>
         {nums.map(number => (
-          <CircleButton className={number === 0 ? zero : undefined} text={number.toString()}></CircleButton>
+          <CircleButton
+            className={number === '0' ? zero : undefined}
+            text={number.toString()}
+            click={() => onNumberPress(number)}
+          />
         ))}
       </div>
     </div>
--- src/lib/pages/Calculator/Calculator.tsx
+++ src/lib/pages/Calculator/Calculator.tsx
@@ -1,19 +1,80 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { ButtonPanel } from '../../organisms/ButtonPanel/ButtonPanel';
-import { useState } from 'react';
 import { Output } from '../../atoms/Output/Output';
 import styles from './Calculator.module.scss';
+import { Op } from './Operations';
 
 const {container} = styles;
 
 export const Calculator = () => {
-  const [output, setOutput] = useState(0);
+  const [output, setOutput] = useState('0');
+  const [num, setNum] = useState('0');
+  const [calc, setCalc] = useState(0);
+  const [op, setOp] = useState(Op.NO_OP);
+
+  const reset = () => {
+    setOutput('0');
+    setCalc(0);
+    setNum('0');
+    setOp(Op.NO_OP);
+  }
+
+  const numPress = (nextInt: string) => {
+    if (op === Op.EQUALS) {
+      reset();
+    }
+
+    let curNum = num.concat(nextInt)
+
+    if (curNum.match(/^\./)) {
+      curNum = `0${curNum}`;
+    }
+    else if (!curNum.match(/^0\./)) {
+      curNum = curNum.replace(/^0/, '');
+    }
+
+    setNum(curNum);
+  }
+
+  const opSelect = (operation: Op) => {
+    switch (op) {
+      case Op.ADD:
+        setCalc(calc + parseFloat(num));
+        break;
+      case Op.SUBTRACT:
+        setCalc(calc - parseFloat(num));
+        break;
+      case Op.DIVIDE:
+        setCalc(calc / parseFloat(num));
+        break;
+      case Op.MULTIPLY:
+        setCalc(calc * parseFloat(num));
+        break;
+      case Op.EQUALS:
+        break;
+      default:
+        setCalc(parseFloat(num));
+    }
+    setOp(operation);
+    setNum('');
+  }
+
+  useEffect(() => {
+    if (op === Op.EQUALS) {
+      setOutput(calc.toString());
+    } else {
+      setOutput(op !== Op.NO_OP ? `${calc}${op}${num}` : num);
+    }
+  }, [calc, op, num]);
+
   return (
     <div className={container}>
       <Output value={output}/>
       <ButtonPanel
-        onOutput={() => {
-        }}
+        onNumberPress={numPress}
+        onReset={reset}
+        onOpSelect={opSelect}
       />
     </div>
   );
