be5d6f5a4428e93f89354769c06b8fbe77c678cd
Mike Deiters <mike@deiters.me>
2023-01-28 20:12:21 -0500

    Quick color changes

--- src/lib/organisms/ButtonPanel/ButtonPanel.tsx
+++ src/lib/organisms/ButtonPanel/ButtonPanel.tsx
@@ -9,22 +9,25 @@ type Props = {
   onReset: () => void;
 };
 
-const {btnPanel, controls, ops, numbers, zero} = styles;
+const {btnPanel, controls, ops, numbers, zero, purple, darkGrey, lightGrey} = styles;
 export const ButtonPanel = ({onNumberPress, onOpSelect, onReset}: Props) => {
   const nums: string[] = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
   return (
     <div className={btnPanel}>
       <div className={controls}>
         <CircleButton
+          className={lightGrey}
           text='AC'
           click={onReset}
         />
         <CircleButton
+          className={lightGrey}
           text='+ / -'
           click={() => {
           }}
         />
         <CircleButton
+          className={lightGrey}
           text='%'
           click={() => {
           }}
@@ -33,29 +36,34 @@ export const ButtonPanel = ({onNumberPress, onOpSelect, onReset}: Props) => {
       <div className={ops}>
         <CircleButton
           text={Op.DIVIDE}
+          className={purple}
           click={() => onOpSelect(Op.DIVIDE)}
         />
         <CircleButton
           text={Op.MULTIPLY}
+          className={purple}
           click={() => onOpSelect(Op.MULTIPLY)}
         />
         <CircleButton
           text={Op.SUBTRACT}
+          className={purple}
           click={() => onOpSelect(Op.SUBTRACT)}
         />
         <CircleButton
           text={Op.ADD}
+          className={purple}
           click={() => onOpSelect(Op.ADD)}
         />
         <CircleButton
           text={Op.EQUALS}
+          className={purple}
           click={() => onOpSelect(Op.EQUALS)}
         />
       </div>
       <div className={numbers}>
         {nums.map(number => (
           <CircleButton
-            className={number === '0' ? zero : undefined}
+            className={[number === '0' ? zero : undefined, darkGrey].join(' ')}
             text={number.toString()}
             click={() => onNumberPress(number)}
           />
